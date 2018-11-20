ebd841776c2527257fd9f723d7538b7f8597b701
Josh Minzner <josh@minzner.org>
2018-11-20 18:01:39 -0500

    Add lots of comments to the code (#16)

--- packages/enzyme-context-utils/src/ValueWatcher.ts
+++ packages/enzyme-context-utils/src/ValueWatcher.ts
@@ -1,19 +1,35 @@
 type Listener<V> = (value: V) => void;
 
+/**
+ * A class that keeps track of a single value and notifies listeners when that value changes.
+ */
 export class ValueWatcher<V> {
   private listeners: Listener<V>[] = [];
 
   value: V | null = null;
 
+  /**
+   * Registers a listener to be notified of value changes
+   *
+   * @param listener a function with accepts the new value as its only arg
+   */
   listen(listener: Listener<V>): void {
     this.listeners.push(listener);
   }
 
+  /**
+   * Change the value. This will cause listeners to be notified.
+   *
+   * @param value
+   */
   set(value: V): void {
     this.value = value;
     this.listeners.forEach(listener => listener(value));
   }
 
+  /**
+   * Removes all listeners
+   */
   stop(): void {
     this.listeners = [];
   }
