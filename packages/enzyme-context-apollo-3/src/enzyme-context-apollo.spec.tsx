94eeaddd1ec49c3551b151150aa79529cd2365c1
Doron Shapiro <DoronShapiro@users.noreply.github.com>
2020-09-23 11:05:52 -0400

    Bump Apollo to 3.x (#40)
    Drops compatibility with Apollo 2.x and tweaks `apolloContext` to be compatible with Apollo 3.x
    
    ### All Submissions:
    
    * [x] Have you followed the guidelines in our Contributing document?
    * [x] Have you checked to ensure there aren't other open [Pull Requests](../../pulls) for the same update/change?
    * [x] Have you added an explanation of what your changes do and why you'd like us to include them?
    * [x] Have you written new tests for your core changes, as applicable?
    * [x] Have you successfully ran tests with your changes locally?

---
+++ packages/enzyme-context-apollo-3/src/enzyme-context-apollo.spec.tsx
@@ -0,0 +1,117 @@
+import { Query } from '@apollo/client/react/components';
+import React, { ReactElement } from 'react';
+import gql from 'graphql-tag';
+import { ReactWrapper, MountRendererProps } from 'enzyme';
+import { createMount, GetContextWrapper, GetOptions } from 'enzyme-context';
+import { apolloContext } from '.';
+
+const GraphQLSchema = gql`
+  type Address {
+    street: String!
+    city: String!
+    state: String!
+    zip: String!
+  }
+
+  type Company {
+    name: String!
+    age: Int
+  }
+
+  type Query {
+    address: Address!
+    company: Company!
+  }
+
+  schema {
+    query: Query
+  }
+`.loc!.source.body;
+
+const TestQuery = gql`
+  query TestQuery {
+    address {
+      street
+    }
+    company {
+      name
+    }
+  }
+`;
+
+const MyComponent: React.SFC = () => {
+  return (
+    <Query query={TestQuery}>
+      {({ data }) => (
+        <div>
+          Street: {data && data.address && data.address.street}
+          <br />
+          Company: {data && data.company && data.company.name}
+        </div>
+      )}
+    </Query>
+  );
+};
+
+describe('enzyme-context-apollo', () => {
+  let mount: (
+    node: ReactElement<any>,
+    options?: GetOptions<MountRendererProps, Plugins>,
+  ) => GetContextWrapper<ReactWrapper, Plugins>;
+  let component: GetContextWrapper<ReactWrapper, Plugins>;
+
+  type Plugins = {
+    client: ReturnType<typeof apolloContext>;
+  };
+
+  beforeEach(async () => {
+    const _mount = createMount({
+      client: apolloContext({
+        schema: {
+          typeDefs: GraphQLSchema,
+        },
+        defaultMocks: {
+          Query: () => ({
+            address: () => ({
+              street: '45 West 11th',
+              city: 'New York',
+              state: 'NY',
+              zip: '10011',
+            }),
+            company: () => ({
+              name: 'TrialSpark',
+              age: null,
+            }),
+          }),
+        },
+      }),
+    });
+    mount = _mount;
+    component = mount(<MyComponent />);
+    await component.client.resetStore();
+    component.update();
+  });
+
+  it('exists', () => {
+    expect(component.exists()).toBe(true);
+    expect(component.text()).toContain('Street: 45 West 11th');
+    expect(component.text()).toContain('Company: TrialSpark');
+  });
+
+  it('supports overriding the default mocks', async () => {
+    component = mount(<MyComponent />, {
+      apolloMocks: {
+        Query: () => ({
+          company: () => ({
+            name: 'Cool Co.',
+            age: null,
+          }),
+        }),
+      },
+    });
+    await component.client.resetStore();
+    component.update();
+    expect(component.text()).toContain('Street: 45 West 11th');
+    expect(component.text()).toContain('Company: Cool Co.');
+  });
+});
---
+++ packages/enzyme-context-apollo-3/src/index.tsx
@@ -0,0 +1,60 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  PossibleTypesMap,
+  NormalizedCacheObject,
+} from '@apollo/client';
+import { SchemaLink } from '@apollo/client/link/schema';
+import React from 'react';
+import { EnzymePlugin, composeWrappingComponents } from 'enzyme-context-utils';
+import {
+  makeExecutableSchema,
+  IExecutableSchemaDefinition,
+  IMocks,
+  addMockFunctionsToSchema,
+} from 'graphql-tools';
+import { defaultMocks } from './Utils';
+
+export type ApolloPluginConfig = {
+  possibleTypes?: PossibleTypesMap;
+  schema: IExecutableSchemaDefinition;
+  defaultMocks?: IMocks;
+};
+
+export type ApolloPluginMountOptions = {
+  apolloMocks?: IMocks;
+};
+
+export const apolloContext: (
+  config: ApolloPluginConfig,
+) => EnzymePlugin<ApolloPluginMountOptions, ApolloClient<NormalizedCacheObject>> = config => (
+  node,
+  options,
+) => {
+  const schema = makeExecutableSchema(config.schema);
+  addMockFunctionsToSchema({
+    schema,
+    // Mocks passed at mount-time and config-time are merged together
+    mocks: defaultMocks(options.apolloMocks || {}, config.defaultMocks || {}),
+    preserveResolvers: true,
+  });
+  const client = new ApolloClient({
+    link: new SchemaLink({ schema }),
+    cache: new InMemoryCache({ possibleTypes: config.possibleTypes }),
+  });
+  const ApolloContextProvider: React.FC = ({ children }) => (
+    <ApolloProvider client={client}>{children}</ApolloProvider>
+  );
+
+  return {
+    node,
+    controller: client,
+    options: {
+      wrappingComponent: composeWrappingComponents(
+        options.wrappingComponent,
+        ApolloContextProvider,
+      ),
+    },
+  };
+};
